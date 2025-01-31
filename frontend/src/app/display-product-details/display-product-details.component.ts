209183ed8198e6e2f06cfc5d7529cc2b9f41fc23
PavishK <kpavish136@gmail.com>
2025-01-31 14:52:11 +0530

    display product completed

--- frontend/src/app/display-product-details/display-product-details.component.ts
+++ frontend/src/app/display-product-details/display-product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
+import { RouterManagerService } from '../router-manager.service';
 
 @Component({
   selector: 'app-display-product-details',
@@ -10,7 +13,11 @@ export class DisplayProductDetailsComponent implements OnInit {
 
   public productData:any=[];
   public makeLoading:boolean=false;
-  constructor(private route:ActivatedRoute){}
+  public allProductData:ProductSchema[]=[];
+  public matchColor: any = { bread: 'red', cake: 'gray', biscuit: 'orange', rusk: 'green' };
+  public popUpState:boolean=false;
+  public popUpDescription:boolean=false;
+  constructor(private route:ActivatedRoute, private toast:ToastrService,private http:HttpClient, private routeMoveTo:RouterManagerService){}
 
   public MakeLoadingVisible():void{
     this.makeLoading=true;
@@ -25,9 +32,44 @@ export class DisplayProductDetailsComponent implements OnInit {
       this.route.params.subscribe(
         params=>{
           this.MakeLoadingVisible();
+          console.log(params);
           this.productData=params;
+
         }
       );
-  }
 
+        this.http.get("http://localhost:8080/product-api/list-products")
+          .subscribe({
+            next: (res: any) => {
+              this.allProductData = res.data;
+              //this.allProductData=this.allProductData.filter(item=> item.type==this.productData.type ) //&& item._id!=this.productData._id
+              console.log(this.allProductData);
+            },
+            error: (err:any) => this.toast.error(err.error.message)
+          });
+      }
+
+      public onClickProduct(data:any):void{
+        this.routeMoveTo.moveWithData("display-product-details/product",data);
+
+      }
+
+      public closeOrOpenStatePopup(){
+        this.popUpState=!this.popUpState;
+      }
+      public closeOrOpenDescriptionPopUp(){
+        this.popUpDescription=!this.popUpDescription;
+      }
+}
+
+
+interface ProductSchema{
+  _id:any;
+  name: string;
+  type: string;
+  price: number;
+  src:string;
+  desc:string;
+  stock_quandity:number;
+  rating:number;
 }
